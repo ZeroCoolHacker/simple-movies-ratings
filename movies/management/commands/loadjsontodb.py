e35cc403791c20d6334cf1ab8fdeeb9430ea8b8c
Shahzaib Akash <shahzaibakash@gmail.com>
2020-06-05 07:23:16 +0500

    load json objects corerctly

--- movies/management/commands/loadjsontodb.py
+++ movies/management/commands/loadjsontodb.py
@@ -1,9 +1,25 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+import json
 
 class Command(BaseCommand):
-    help = 'Displays current time'
+    help = 'Loads the provided movie.json data into the database'
 
     def handle(self, *args, **kwargs):
-        time = timezone.now().strftime('%X')
-        self.stdout.write("It's now %s" % time)
+        filename = 'movies.json'
+        with open(filename) as f:
+            data = json.load(f)
+            for movie in data:
+                print(f"id: {movie['id']}")
+                print(f"name: {movie['name']}")
+                print(f"description: {movie['description']}")
+                print(f"imgPath: {movie['imgPath']}")
+                print(f"duration: {movie['duration']}")
+                for genre in movie['genre']:
+                    print(f"genre: {genre}")
+                print(f"language: {movie['language']}")
+                print(f"mpaaRating-type: {movie['mpaaRating']['type']}")
+                print(f"mpaaRating-label: {movie['mpaaRating']['label']}")
+                print(f"userRating: {movie['userRating']}")
+                
+                
