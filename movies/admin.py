4e7484c84b8e3a9c7eafdfcbd93ce29d42c8d8bd
Shahzaib Akash <shahzaibakash@gmail.com>
2020-06-05 07:12:37 +0500

    add admin views

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Movie, MPAA_Rating, Genre
 
 # Register your models here.
+admin.site.register(MPAA_Rating)
+admin.site.register(Genre)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    '''Admin View for Movie'''
+
+    # list_display = ('',)
+    # list_filter = ('',)
+    # inlines = [
+    #     Inline,
+    # ]
+    # raw_id_fields = ('',)
+    # readonly_fields = ('',)
+    # search_fields = ('',)
+    # date_hierarchy = ''
+    # ordering = ('',)
